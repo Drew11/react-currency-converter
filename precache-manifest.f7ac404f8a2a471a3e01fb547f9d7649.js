e90ed24aa64559e688a8793deeea22c579a935fa
Drew11 <drew.naumenko@gmail.com>
2019-11-15 10:24:33 +0200

    Updates

---
+++ precache-manifest.f7ac404f8a2a471a3e01fb547f9d7649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a7863cadfd0047636fb7f61e37cf82",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "cfdb47ade8fe65de28ea",
+    "url": "/react-currency-converter/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "2958be536ec41c0329e3",
+    "url": "/react-currency-converter/static/css/main.8b69c555.chunk.css"
+  },
+  {
+    "revision": "cfdb47ade8fe65de28ea",
+    "url": "/react-currency-converter/static/js/2.25581239.chunk.js"
+  },
+  {
+    "revision": "2958be536ec41c0329e3",
+    "url": "/react-currency-converter/static/js/main.e11b6f72.chunk.js"
+  },
+  {
+    "revision": "4f7421116c1f507bf08f",
+    "url": "/react-currency-converter/static/js/runtime-main.3b96fbb5.js"
+  }
+]);
