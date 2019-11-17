04798eeecd9addca9a693738f1de60c7fa3d3250
Drew11 <drew.naumenko@gmail.com>
2019-11-17 11:21:43 +0200

    fix path

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import CurrencyRates from './components/CurrencyRates/CurrencyRates';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Nav} from 'react-bootstrap';
 import {Route, Switch,} from "react-router";
-import {HashRouter} from "react-router-dom";
 import './App.css';
 
 const App = () => {
     return (
-        <HashRouter basename="/react-currency-converter/">
             <div className="App">
                 <header className="App-header">
 
                     <Nav variant="tabs" defaultActiveKey="/home">
                         <Nav.Item>
-                            <Nav.Link href="/">Converter</Nav.Link>
+                            <Nav.Link href="./">Converter</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/currency_rates">
+                            <Nav.Link href="./currency_rates">
                                 Currency Rates
                             </Nav.Link>
                         </Nav.Item>
@@ -28,14 +26,13 @@ const App = () => {
 
                 <main>
                     <Switch>
-                        <Route exact path="/" component={Converter}/>
-                        <Route path="/currency_rates" component={CurrencyRates}/>
+                        <Route  exact path="/" component={Converter}/>
+                        <Route  path="/currency_rates/" component={CurrencyRates}/>
                     </Switch>
                 </main>
                 <footer>
                 </footer>
             </div>
-        </HashRouter>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <Router>
+    <Router basename="/react-currency-converter/">
         <App />
     </Router>,
     document.getElementById('root'));
